4ca30c857e307359bd5e4dab1a521ab735932828
Stanley Shyiko <stanley.shyiko@gmail.com>
2016-10-01 20:34:57 -0700

    Made link/unlink behaviour consistent with install/uninstall

--- command/link.go
+++ command/link.go
@@ -3,22 +3,31 @@ package command
 import (
 	"errors"
 	"github.com/shyiko/jabba/cfg"
+	"github.com/shyiko/jabba/semver"
 	"path/filepath"
 	"os"
 	"strings"
 )
 
-func Link(name string, dir string) error {
-	if !strings.HasPrefix(name, "system@") {
+func Link(selector string, dir string) error {
+	if !strings.HasPrefix(selector, "system@") {
 		return errors.New("Name must begin with 'system@' (e.g. 'system@1.8.73')")
 	}
+	// <version> has to be valid per semver
+	if _, err := semver.ParseVersion(selector); err != nil {
+		return err
+	}
 	if dir == "" {
-		return os.Remove(filepath.Join(cfg.Dir(), "jdk", name))
+		ver, err := LsBestMatch(selector)
+		if err != nil {
+			return err
+		}
+		return os.Remove(filepath.Join(cfg.Dir(), "jdk", ver))
 	} else {
 		if err := assertJavaDistribution(dir); err != nil {
 			return err
 		}
-		return os.Symlink(dir, filepath.Join(cfg.Dir(), "jdk", name))
+		return os.Symlink(dir, filepath.Join(cfg.Dir(), "jdk", selector))
 	}
 }
 
