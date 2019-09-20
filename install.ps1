3739d666a57fea6e0f07eae91057527a90c83859
Nikolas Mavropoylos <ProIcons@users.noreply.github.com>
2019-09-20 22:52:13 +0300

    Create Windows jabba.cmd
    It creates Windows jabba.cmd that calls the powershell script, and adds this script permanently to User's environment variables

--- install.ps1
+++ install.ps1
@@ -59,6 +59,10 @@ function jabba
 }
 "@ > $jabbaHome/jabba.ps1
 
+echo "@powershell -command jabba %* > $jabbaHome/jabba.cmd
+
+[Environment]::SetEnvironmentVariable("Path",[Environment]::GetEnvironmentVariable("Path", [System.EnvironmentVariableTarget]::User) + ";" + $jabbaHome,[System.EnvironmentVariableTarget]::User)
+
 $sourceJabba="if (Test-Path `"$jabbaHome\jabba.ps1`") { . `"$jabbaHome\jabba.ps1`" }"
 
 if (-not $(Test-Path $profile))
