9ff60cceb12b3ae935189b95f4ccf29140c7195b
Tsunenobu Kai <kai2nenobu@gmail.com>
2019-09-26 03:03:32 +0900

    Add tests for "use" and "deactivate" command in windows

--- command/use_test.go
+++ command/use_test.go
@@ -19,6 +19,9 @@ func (f FileInfoMock) IsDir() bool        { return true }
 func (f FileInfoMock) Sys() interface{}   { return nil }
 
 func TestUse(t *testing.T) {
+	if runtime.GOOS == "windows" {
+		t.Skip("Unavailable in windows.")
+	}
 	prevPath := os.Getenv("PATH")
 	defer func() { os.Setenv("PATH", prevPath) }()
 	var prevReadDir = readDir
@@ -42,6 +45,35 @@ func TestUse(t *testing.T) {
 		"export PATH=\"" + cfg.Dir() + "/jdk/1.7.2" + suffix + "/bin:/usr/local/bin:/usr/bin\"",
 		"export JAVA_HOME=\"" + cfg.Dir() + "/jdk/1.7.2" + suffix + "\"",
 		"export JAVA_HOME_BEFORE_JABBA=\"/system-jdk\"",
+	}
+	if !reflect.DeepEqual(actual, expected) {
+		t.Fatalf("actual: %v != expected: %v", actual, expected)
+	}
+}
+
+func TestUseInWindows(t *testing.T) {
+	if runtime.GOOS != "windows" {
+		t.Skip("Unavailable except in windows.")
+	}
+	prevPath := os.Getenv("PATH")
+	defer func() { os.Setenv("PATH", prevPath) }()
+	var prevReadDir = readDir
+	defer func() { readDir = prevReadDir }()
+	readDir = func(dirname string) ([]os.FileInfo, error) {
+		return []os.FileInfo{
+			FileInfoMock("1.6.0"), FileInfoMock("1.7.0"), FileInfoMock("1.7.2"), FileInfoMock("1.8.0"),
+		}, nil
+	}
+	os.Setenv("PATH", `C:\Windows\System32;`+cfg.Dir()+`\jdk\1.6.0\bin;C:\Windows`)
+	os.Setenv("JAVA_HOME", `C:\system-jdk`)
+	actual, err := Use("1.7")
+	if err != nil {
+		t.Fatalf("err: %v", err)
+	}
+	expected := []string{
+		`export PATH="` + cfg.Dir() + `\jdk\1.7.2\bin;C:\Windows\System32;C:\Windows"`,
+		`export JAVA_HOME="` + cfg.Dir() + `\jdk\1.7.2"`,
+		`export JAVA_HOME_BEFORE_JABBA="C:\system-jdk"`,
 	}
 	if !reflect.DeepEqual(actual, expected) {
 		t.Fatalf("actual: %v != expected: %v", actual, expected)
